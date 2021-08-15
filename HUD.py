2c489c39e71d6550b0c106273ce21c9eae6d036b
Xilbram <gibram.g.f@gmail.com>
2021-08-15 01:21:02 -0300

    Atributos podem e devem ser alterados. Mudei o programa Tiro para Tiro 2 pois não estava conseguindo fazer funcionar com o original, mas se alguem quiser sinta-se na liberdade para alterar
    Loja não foi testada, visto que não instalei inimigos no arquivo main

--- HUD.py
+++ HUD.py
@@ -1,37 +1,59 @@
 import os
 import pygame
-from abc import ABC
-import time
 pygame.font.init()
+from JogadorNave import Jogador
 
 
-class HUD(ABC):
+class HUD(pygame.sprite.Sprite):
     def __init__(self):
-        #vida e nível não serão definidos aqui, mas deixei assim para testar
-        self.vida_jogador = 5
-        self.nivel = 1
-        #fonte como arial não é definitivo, só teste
-        self.fonte_letreiros = pygame.font.SysFont('Arial',25)
-        self.largura_janela = 800
-        self.altura_janela = 600
-        self.janela = pygame.display.set_mode((self.largura_janela, self.altura_janela))
+        super().__init__()
+        self.fonte_letreiros = pygame.font.SysFont('comicssans',25)
+        self.__largura_janela = 800
+        self.__altura_janela = 600
+        self.janela = pygame.display.set_mode((self.__largura_janela, self.__altura_janela))
         self.tempo_fase = 0
-        self.fundo_tela = pygame.transform.scale(pygame.image.load(os.path.join('Imagens/space_background.jpg')), (self.largura_janela,self.altura_janela))
+        self.fundo_tela = pygame.transform.scale(pygame.image.load(os.path.join('Imagens/space_background.jpg')), (self.__largura_janela, self.__altura_janela))
+        self.display_jogo = pygame.display.set_mode((self.__largura_janela,self.__altura_janela))
+
+    @property
+    def altura_tela(self):
+        return self.__altura_janela
+    @altura_tela.setter
+    def altura_tela(self, altura):
+        self.__altura_janela = altura
+    @property
+    def largura_tela(self):
+        return self.__largura_janela
+    @largura_tela.setter
+    def largura_tela(self, largura):
+        self.__largura_janela = largura
+
 
     #calculo da posição X de onde mostrar a vida é baseado no tamanho da largura da janela, então não tem problema mexer na altura/largura
-    def mostrar_nivel(self):
-        nivel_atual_display = self.fonte_letreiros.render("Nível: {}".format(self.nivel), True, (255,255,255))
-        return self.janela.blit(nivel_atual_display, (self.largura_janela - nivel_atual_display.get_width(), 5))
+    def mostrar_nivel(self, nivel):
+        nivel_atual_display = self.fonte_letreiros.render("Nível: {}".format(nivel), True, (255,255,255))
+        return self.janela.blit(nivel_atual_display, (self.__largura_janela - nivel_atual_display.get_width(), 10))
 
-    def mostrar_vida(self):
-        vida_jogador_display = self.fonte_letreiros.render("Vidas: {}".format(self.vida_jogador), True, (255,0,0))
-        return self.janela.blit(vida_jogador_display, (10,5))
+    #def mostrar_vida(self, jogador: Jogador):
+        #if isinstance(jogador, Jogador):
+            #vida_jogador_display = self.fonte_letreiros.render("Vida {}".format(jogador), True, (255, 0, 0))
+            #return self.janela.blit(vida_jogador_display, (0, 10))
 
-    def mostrar_tempo(self):
-        pass
+    def mostrar_vida(self, jogador: Jogador):
+        #if isinstance(jogador, Jogador):
+        fonte = pygame.font.SysFont('comic sans', 30)
+        mensagem = fonte.render("Vida: " + str(jogador.vida), True, (255, 0,0))
+        self.display_jogo.blit(mensagem, (0, 10))
 
-    def mostrar_inimigos_restantes(self):
-        pass
+    def mostrar_tempo(self, tempo_tela, contagem):
+        fonte = pygame.font.SysFont('comic sans', 30)
+        mensagem = fonte.render("Tempo: " + str(tempo_tela - int(contagem)), True, (255, 255, 255))
+        self.display_jogo.blit(mensagem, (90, 10))
+
+    def mostrar_inimigos_restantes(self, qtd_inimigos):
+        fonte = pygame.font.SysFont('comic sans', 30)
+        mensagem = fonte.render("Inimigos: " + str(qtd_inimigos), True, (255, 255, 255))
+        self.display_jogo.blit(mensagem, (210, 10))
 
     def mostrar_fundo(self):
-        return self.janela.blit(self.fundo_tela, (0,0))
+        return self.janela.blit(self.fundo_tela, (0, 0))
--- Jogo.py
+++ Jogo.py
@@ -3,6 +3,7 @@ import os
 import time
 import random
 import HUD
+from JogadorNave import Jogador
 
 
 class Jogo:
@@ -10,9 +11,12 @@ class Jogo:
         self.is_rodando = True
         self.is_pausado = False
         self.tempo_fase = 0
-        self.FPS = 0
+        self.FPS = 60
         self.tempo = pygame.time.Clock()
         self.hud = HUD.HUD()
+        self.jogador = Jogador((self.hud.largura_tela/2,self.hud.altura_tela),self.hud.largura_tela, self.hud.altura_tela, 5, 100, 0, 0)
+        jogador_sprite = Jogador((self.hud.largura_tela/2,self.hud.altura_tela),self.hud.largura_tela, self.hud.altura_tela, 5, 100, 0, 0)
+        self.jogador_sprite = pygame.sprite.GroupSingle(jogador_sprite)
 
     def pausar(self):
         self.is_pausado = True
@@ -24,9 +28,13 @@ class Jogo:
         while self.is_rodando:
             self.tempo.tick(self.FPS)
             self.hud.mostrar_fundo()
-            self.hud.mostrar_nivel()
-            self.hud.mostrar_vida()
+            self.hud.mostrar_nivel(1)
+            self.hud.mostrar_vida(self.jogador.vida)
+            self.jogador_sprite.update()
+            self.jogador_sprite.draw(surface=self.hud.janela)
+            self.jogador_sprite.sprite.tiros.draw(self.hud.janela)
             pygame.display.update()
+
 
             #sair do jogo quando usuario quitar
             for event in pygame.event.get():
@@ -37,7 +45,6 @@ class Jogo:
 
 jogo1 = Jogo()
 jogo1.loop_jogo()
-
 
 
 
